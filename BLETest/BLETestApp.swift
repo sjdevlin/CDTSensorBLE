21daa54db55ec3d426593113179c36dca7d90bb4
sjdevlin <sjdevlin@gmail.com>
2022-12-24 20:29:06 +0000

    Initial Commit

---
+++ BLETest/BLETestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLETestApp.swift
+//  BLETest
+//
+//  Created by Stephen Devlin on 24/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct BLETestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
